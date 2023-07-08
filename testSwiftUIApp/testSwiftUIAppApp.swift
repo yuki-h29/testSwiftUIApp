51794d84fcb08c42e82fd1d2c288aa95b32fd07f
yuki-h29 <yuki.hirano29@gmail.com>
2023-07-08 14:58:07 +0900

    Initial Commit

---
+++ testSwiftUIApp/testSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIAppApp.swift
+//  testSwiftUIApp
+//
+//  Created by 平野裕貴 on 2023/07/08.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
